c8970db2ade1e3277c68d62265edb50d4e385bf9
Ryan Rice <ryanr1230@gmail.com>
2015-11-05 17:07:28 -0800

    generating graph for first store

--- src/js/movement.js
+++ src/js/movement.js
@@ -1,12 +1,15 @@
 var perspective_movement = {
   x_velocity: 0,
-  y_velocity: 0
+  y_velocity: 0,
+  z_velocity: 0
 };
 
 var W = 87,
     A = 65,
     S = 83,
-    D = 68;
+    D = 68,
+    DOWN_ARROW = 40,
+    UP_ARROW = 38;
 
 window.onkeydown = function(e) {
     switch(e.keyCode) {
@@ -14,6 +17,9 @@ window.onkeydown = function(e) {
       case A: perspective_movement.x_velocity = -1; break;
       case S: perspective_movement.y_velocity = -1; break;
       case D: perspective_movement.x_velocity = 1; break;
+      case UP_ARROW: perspective_movement.z_velocity = -1; break;
+      case DOWN_ARROW: perspective_movement.z_velocity = 1; break;
+
     }
 }
 
@@ -24,5 +30,7 @@ window.onkeyup = function(e) {
       case A: perspective_movement.x_velocity = 0; break;
       case S: perspective_movement.y_velocity = 0; break;
       case D: perspective_movement.x_velocity = 0; break;
+      case UP_ARROW: perspective_movement.z_velocity = 0; break;
+      case DOWN_ARROW: perspective_movement.z_velocity = 0; break;
     }
 }
