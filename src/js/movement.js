89f216810380d1235590effd8d6698c45cc18e72
Ryan Rice <ryanr1230@gmail.com>
2015-11-05 16:25:40 -0800

    adding movement

---
+++ src/js/movement.js
@@ -0,0 +1,28 @@
+var perspective_movement = {
+  x_velocity: 0,
+  y_velocity: 0
+};
+
+var W = 87,
+    A = 65,
+    S = 83,
+    D = 68;
+
+window.onkeydown = function(e) {
+    switch(e.keyCode) {
+      case W: perspective_movement.y_velocity = 1; break;
+      case A: perspective_movement.x_velocity = -1; break;
+      case S: perspective_movement.y_velocity = -1; break;
+      case D: perspective_movement.x_velocity = 1; break;
+    }
+}
+
+
+window.onkeyup = function(e) {
+    switch(e.keyCode) {
+      case W: perspective_movement.y_velocity = 0; break;
+      case A: perspective_movement.x_velocity = 0; break;
+      case S: perspective_movement.y_velocity = 0; break;
+      case D: perspective_movement.x_velocity = 0; break;
+    }
+}
